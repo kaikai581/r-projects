e1dcb34a3cc06019c84300675941139999d681be
Shih-Kai Lin <kaikai581@hotmail.com>
2018-11-02 17:56:04 -0500

    Trying to fit to model.

--- wrong_sign_mixture_model/lda/fit_single_skew_t.R
+++ wrong_sign_mixture_model/lda/fit_single_skew_t.R
@@ -1,8 +1,19 @@
 # load file
-data<-read.csv('lda_value_target_flattened.csv', sep=' ', header=FALSE)
+all_data<-read.csv('lda_value_target_flattened.csv', sep=' ', header=FALSE)
+data<-all_data[seq(1, nrow(all_data), 100),]
 
 # select neutrino events
 ldv = subset(data, V2 == 14)
 
 # select antineutrino events
 ldvbar = subset(data, V2 == -14)
+
+# plot histogram of neutrino events
+hist(ldv$V1)
+
+# fit skew t
+library(EMMIXcskew)
+#Fit<-fmcfust(1, ldv$V1)
+Fit<-dcfust(ldv$V1, delta=rep(0, nrow(ldv)))
+summary(Fit)
+print(Fit)
